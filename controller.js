641abaa9f136474b565fb263159ba0cddcf03e73
Arthur Viegers <arthur@mongodb.com>
2014-12-17 22:33:46 +0100

    Log process activity

--- controller.js
+++ controller.js
@@ -111,9 +111,9 @@ app.use(express.static(__dirname + "/public"));
 // GET requests
 app.get("/api/action", function(request, response) {
 	switch (request.query.type) {
-	case "restart":		aiota.restartProcess(request.query.process, config.serverName, request.query.pid);
+	case "restart":		aiota.restartProcess(request.query.process, config.serverName, request.query.pid, db);
 						break;
-	case "stop":		aiota.stopProcess(request.query.process, config.serverName, request.query.pid);
+	case "stop":		aiota.stopProcess(request.query.process, config.serverName, request.query.pid, db);
 						break;
 	case "kill":		aiota.killProcess(request.query.pid);
 						break;
